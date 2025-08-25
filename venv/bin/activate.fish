5d1dce4f43857d562342d9d5a2073198c87d8fa5
Manuel Velazquez <manuelvelazquez@MacBookAir.attlocal.net>
2025-08-24 21:53:56 -0700

    Initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/manuelvelazquez/Desktop/weather-api-dashboard/venv"
+set -gx VIRTUAL_ENV "/Users/manuelvelazquez/Desktop/dataresumes/weather-api-dashboard-v2/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
